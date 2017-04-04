62c2f41e583fbfc26227b1e4200de7d9c29f8afa
jmei8386 <jmei8686@gmail.com>
2017-04-04 16:04:48 +0800

    Regular auto-commit

---
+++ autogit.bat
@@ -0,0 +1,5 @@
+﻿
+cd E:\估價單\
+git add -A
+
+git commit -m "Regular auto-commit"
--- autogit.sh
+++
@@ -1,7 +0,0 @@
-﻿#!/bin/sh
-cd /e/估價單/
-git add -A
-timestamp() {
-  date +"at %H:%M:%S on %d/%m/%Y"
-}
-git commit -m "Regular auto-commit $(timestamp)"
